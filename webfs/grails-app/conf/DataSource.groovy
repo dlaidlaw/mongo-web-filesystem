22f2ed6787b86a003ee9f7be6c2717b79c93d5b7
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2012-05-17 14:52:30 +0300

    Separate mongo datasource for different environments.

--- webfs/grails-app/conf/DataSource.groovy
+++ webfs/grails-app/conf/DataSource.groovy
@@ -1,11 +1,41 @@
-grails {
-    mongo {
-        host = "mongodb"
-        port = 27017
-        databaseName = "webfs"
-		options {
-			autoConnectRetry = true
-			connectTimeout = 300
-		}
+environments {
+    development {
+        grails {
+            mongo {
+                host = "mongodb"
+                port = 27017
+                databaseName = "webfs"
+                options {
+                    autoConnectRetry = true
+                    connectTimeout = 300
+                }
+            }
+        }
+    }
+    test {
+        grails {
+            mongo {
+                host = "mongodb"
+                port = 27017
+                databaseName = "webfs_test"
+                options {
+                    autoConnectRetry = true
+                    connectTimeout = 300
+                }
+            }
+        }
+    }
+    production {
+        grails {
+            mongo {
+                host = "mongodb"
+                port = 27017
+                databaseName = "webfs"
+                options {
+                    autoConnectRetry = true
+                    connectTimeout = 300
+                }
+            }
+        }
     }
 }
