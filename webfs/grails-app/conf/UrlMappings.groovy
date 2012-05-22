69ca33d990636bfa1fbc57ec451ad182f8da19cd
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2012-05-22 15:52:08 +0300

    Cleanup url mappings.

--- webfs/grails-app/conf/UrlMappings.groovy
+++ webfs/grails-app/conf/UrlMappings.groovy
@@ -1,9 +1,13 @@
 class UrlMappings {
 
 	static mappings = {
-		"/v1.0/$tenant/$id?"(controller: "filesystem", parseRequest: false) {
-			action = [GET: "get", PUT: "update", DELETE: "delete", POST: "insert"]
+		"/v1.0/$tenant/?"(controller: "filesystem", parseRequest: false) {
+			action = [GET: "get", POST: "insert"]
 		}
+
+        "/v1.0/$tenant/$id/?"(controller: "filesystem", parseRequest: false) {
+            action = [GET: "get", PUT: "update", DELETE: "delete"]
+        }
 
 		"/"(view:"/index")
 		"500"(view:'/error')
